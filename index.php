7bb2dbbd92e4ae8657d7ff66e37063fa6480885c
Mike Simmonds <mike@simmo.me>
2014-12-11 08:30:34 +0000

    Employment data moved to array

--- index.php
+++ index.php
@@ -46,7 +46,41 @@ $app->get('/', function() use ($app) {
     array('name' => 'cross-browser/legacy testing', 'total' => '98%')
   );
   
-  $app->render('index.html', array('projects' => $projects, 'skills' => $skills));
+  $employment = array(
+    array(
+      'company' => 'Caspian Media',
+      'start_date' => strtotime('2009/03/16'),
+      'end_date' => strtotime('2014/09/26'),
+      'description' => 'Lead developer',
+      'recommendations' => 'http://lnkd.in/2T4KbK',
+      'achievements' => array(
+        'Designed and built a custom CMS that has been in use for over 5 years and manages Caspian\'s web portfolio',
+        'Created bespoke PHP framework that runs the CMS and all sites/applications in Caspian\'s web portfolio',
+        'Responsively designed and built Real Business - one of the UK\'s largest, ad driven online magazine for entrepreneurs',
+        'Designed and built Real Deals, currently the only responsively designed private equity magazine',
+        'Rebuilt Caspian\'s entire web portfolio to be responsive',
+        'Set business up with version control, initally Subversion then over to Git/GitHub',
+        'Integration with third party systems - Most notable being Workbooks (CRM) and Bureau van Dijk\'s Amadeus (financial information for public and private companies)',
+        'Moved web portfolio to Amazon Web Services',
+        'Managing client expectations'
+      )
+    ),
+    array(
+      'company' => 'Sainsbury\'s',
+      'start_date' => strtotime('2002/08/01'),
+      'end_date' => strtotime('2009/03/14'),
+      'description' => 'Online co-ordinator',
+      'recommendations' => null,
+      'achievements' => array(
+        'Learning how to manage a large, diverse, team with different wants and needs',
+        'Dealing with customers',
+        'Learning how to take a process, refine it and how to deploy change',
+        'Organisation and efficiency'
+      )
+    )
+  );
+
+  $app->render('index.html', array('projects' => $projects, 'skills' => $skills, 'employment' => $employment));
 });
 
 $app->get('/mr-dixons-tea', function() use ($app) {
