2baa033c951bbf3f245802508c9c10e2a2eb17ba
Mike Simmonds <mike@simmo.me>
2016-03-14 22:10:50 +0000

    Data format cleanup

--- data.js
+++ data.js
@@ -1,142 +1,185 @@
 export default {
     projects: [
         {
-            'name': 'Virgin: Pitch to Rich',
-            'url': 'http://www.virginmediabusiness.co.uk/pitch-to-rich',
-            'employer': 'Zone',
+            name: 'Virgin: Pitch to Rich',
+            url: 'http://www.virginmediabusiness.co.uk/pitch-to-rich',
+            employer: 'Zone',
             description: 'A nationally marketed campaign where entrants pitch business ideas to Richard Branson. The winner receives £1m of investment. Kickstarter meets Dragon\'s Den.',
-            'cta': 'Pitch to Rich',
-            'image': {
-                'url': 'virgin.jpg',
-                'width': 700,
-                'height': 492
+            cta: 'Pitch to Rich',
+            image: {
+                url: 'virgin.jpg',
+                width: 700,
+                height: 492
             }
         },
         {
-            'name': 'BBC: Audio/Visual',
-            'url': false,
-            'employer': 'Zone',
+            name: 'BBC: Audio/Visual',
+            url: false,
+            employer: 'Zone',
             description: 'Building high fidelity, next generation, HTML5 audio/video prototypes as well as User Interface/User Experience consultancy for the BBC\'s AVKX team.',
-            'image': {
-                'url': 'testcard.jpg',
-                'width': 700,
-                'height': 492
+            image: {
+                url: 'testcard.jpg',
+                width: 700,
+                height: 492
             }
         },
         {
-            'name': 'Prezzo',
-            'url': 'http://www.prezzorestaurants.co.uk',
-            'employer': 'Zone',
+            name: 'Prezzo',
+            url: 'http://www.prezzorestaurants.co.uk',
+            employer: 'Zone',
             description: 'Responsive site and email build for one of the UK\'s largest restaurant chains.',
-            'image': {
-                'url': 'prezzo.jpg',
-                'width': 700,
-                'height': 492
+            image: {
+                url: 'prezzo.jpg',
+                width: 700,
+                height: 492
             }
         },
         {
-            'name': 'Real Business',
-            'url': 'http://realbusiness.co.uk',
-            'employer': 'Caspian Media',
+            name: 'Real Business',
+            url: 'http://realbusiness.co.uk',
+            employer: 'Caspian Media',
             description: 'Responsive design and build for an ad-driven site with a large user base.',
-            'image': {
-                'url': 'real-business.jpg',
-                'width': 700,
-                'height': 494
+            image: {
+                url: 'real-business.jpg',
+                width: 700,
+                height: 494
             }
         },
         {
-            'name': 'Real Deals',
-            'url': 'http://realdeals.eu.com',
-            'employer': 'Caspian Media',
+            name: 'Real Deals',
+            url: 'http://realdeals.eu.com',
+            employer: 'Caspian Media',
             description: 'Responsive design – a first for the private equity industry – with a focus on legacy browser support.',
-            'image': {
-                'url': 'real-deals.jpg',
-                'width': 700,
-                'height': 492
+            image: {
+                url: 'real-deals.jpg',
+                width: 700,
+                height: 492
             }
         },
         {
-            'name': 'Atkins: Angles',
-            'url': 'http://angles.atkinsglobal.com',
-            'employer': 'Caspian Media',
+            name: 'Atkins: Angles',
+            url: 'http://angles.atkinsglobal.com',
+            employer: 'Caspian Media',
             description: 'Responsive web app, built on Wordpress, making use of HTML5 cache and history APIs for super-fast loading and offline portfolio.',
-            'cta': 'Angles',
-            'image': {
-                'url': 'angles.jpg',
-                'width': 700,
-                'height': 494
+            cta: 'Angles',
+            image: {
+                url: 'angles.jpg',
+                width: 700,
+                height: 494
             }
         },
         {
-            'name': 'Oktra',
-            'url': 'http://oktra.co.uk',
-            'employer': 'Draw (Freelance)',
+            name: 'Oktra',
+            url: 'http://oktra.co.uk',
+            employer: 'Draw (Freelance)',
             description: 'Part of the team responsible for delivering a front end build for the UK\'s leading office interior\'s firm.',
-            'image': {
-                'url': 'oktra.jpg',
-                'width': 700,
-                'height': 459
+            image: {
+                url: 'oktra.jpg',
+                width: 700,
+                height: 459
             }
         }
     ],
     recommendations: [
         {
-            'quote': 'Mike is an encouraging team mate and <mark>fearlessly forward thinking</mark>. He has a <mark>great eye for detail</mark>, a <mark>good understanding of design</mark> and more importantly, <mark>Mike builds websites that work well</mark>! It\'s a pleasure to work with him.',
-            'author': 'Amy Willoughby',
-            'role': 'Senior Designer at SapientNitro',
-            'photo': 'amy-willoughby.jpg'
+            quote: 'Mike is an encouraging team mate and <mark>fearlessly forward thinking</mark>. He has a <mark>great eye for detail</mark>, a <mark>good understanding of design</mark> and more importantly, <mark>Mike builds websites that work well</mark>! It\'s a pleasure to work with him.',
+            author: 'Amy Willoughby',
+            role: 'Senior Designer at SapientNitro',
+            photo: 'amy-willoughby.jpg'
         },
         {
-            'quote': 'Mike is a top developer! <mark>Highly skilled, very knowledge, methodical and a genuinely nice guy</mark> - He is a very well rounded developer and has great attitude to life, learning and new technologies. Mike was <mark>a great mentor</mark> to me, encouraging me to push myself whilst offering great support and <mark>brilliant problem solving skills</mark> and techniques. I can highly recommend Mike for all things web related!',
-            'author': 'Luke Zammit',
-            'role': 'Front End/UI Developer at Macmillan',
-            'photo': 'luke-zammit.jpg'
+            quote: 'Mike is a top developer! <mark>Highly skilled, very knowledge, methodical and a genuinely nice guy</mark> - He is a very well rounded developer and has great attitude to life, learning and new technologies. Mike was <mark>a great mentor</mark> to me, encouraging me to push myself whilst offering great support and <mark>brilliant problem solving skills</mark> and techniques. I can highly recommend Mike for all things web related!',
+            author: 'Luke Zammit',
+            role: 'Front End/UI Developer at Macmillan',
+            photo: 'luke-zammit.jpg'
         },
         {
-            'quote': 'Mike is an incredibly <mark>capable</mark> and <mark>creative</mark> developer. Always <mark>eager</mark> and capable of taking on new challenges he is <mark>constantly learning</mark> and improving his skill set. His ability to <mark>solve problems quickly</mark> with <mark>simple and elegant solutions</mark> makes him <mark>an incredible asset to any dev team</mark>.',
-            'author': 'Dean Faulkner',
-            'role': 'Technical Director at Draw',
-            'photo': 'dean-faulkner.jpg'
+            quote: 'Mike is an incredibly <mark>capable</mark> and <mark>creative</mark> developer. Always <mark>eager</mark> and capable of taking on new challenges he is <mark>constantly learning</mark> and improving his skill set. His ability to <mark>solve problems quickly</mark> with <mark>simple and elegant solutions</mark> makes him <mark>an incredible asset to any dev team</mark>.',
+            author: 'Dean Faulkner',
+            role: 'Technical Director at Draw',
+            photo: 'dean-faulkner.jpg'
         },
         {
-            'quote': 'Mike is <mark>a brilliant developer and a technical genius</mark>. Watching him at work with a CMS system was <mark>like watching a magician</mark>. It was a pleasure to work with Mike and I would recommend him in a heartbeat.',
-            'author': 'Kate Bassett',
-            'role': 'Features editor at Management Today, Haymarket',
-            'photo': 'kate-bassett.jpg'
+            quote: 'Mike is <mark>a brilliant developer and a technical genius</mark>. Watching him at work with a CMS system was <mark>like watching a magician</mark>. It was a pleasure to work with Mike and I would recommend him in a heartbeat.',
+            author: 'Kate Bassett',
+            role: 'Features editor at Management Today, Haymarket',
+            photo: 'kate-bassett.jpg'
         },
         {
-            'quote': 'Mike is a <mark>fantastic developer</mark>. When we worked together at Caspian Media, he was always able to <mark>fix any problem</mark> we may have had with our content management system. I\'m a non-techie, but Mike was always <mark>patient</mark> and <mark>happy to help</mark>. I would not hesitate to <mark>recommend him to anyone</mark>, and certainly hope to work with him again in the future.',
-            'author': 'Jason Hesse',
-            'role': 'Freelance business journalist and copywriter',
-            'photo': 'jason-hesse.jpg'
+            quote: 'Mike is a <mark>fantastic developer</mark>. When we worked together at Caspian Media, he was always able to <mark>fix any problem</mark> we may have had with our content management system. I\'m a non-techie, but Mike was always <mark>patient</mark> and <mark>happy to help</mark>. I would not hesitate to <mark>recommend him to anyone</mark>, and certainly hope to work with him again in the future.',
+            author: 'Jason Hesse',
+            role: 'Freelance business journalist and copywriter',
+            photo: 'jason-hesse.jpg'
         },
         {
-            'quote': 'Mike is <mark>undoubtedly very good at his job</mark> and I would not hesitate to recommend him to anyone. I worked with Mike on a number of projects at Caspian Media and found him to be <mark>efficient</mark>, <mark>reliable</mark> and <mark>easy to work with</mark>.',
-            'author': 'Aneliese Seaman',
-            'role': 'Marketing Manager at Financial Times',
-            'photo': 'aneliese-seaman.jpg'
+            quote: 'Mike is <mark>undoubtedly very good at his job</mark> and I would not hesitate to recommend him to anyone. I worked with Mike on a number of projects at Caspian Media and found him to be <mark>efficient</mark>, <mark>reliable</mark> and <mark>easy to work with</mark>.',
+            author: 'Aneliese Seaman',
+            role: 'Marketing Manager at Financial Times',
+            photo: 'aneliese-seaman.jpg'
         }
     ],
     skills: [
-        {'name': 'PHP', 'total': '95%'},
-        {'name': 'Ruby/Ruby on Rails', 'total': '60%'},
-        {'name': 'HTML5/CSS', 'total': '99%'},
-        {'name': 'JavaScript', 'total': '80%'},
-        {'name': 'Linux/Apache', 'total': '70%'},
-        {'name': 'MySQL', 'total': '85%'},
-        {'name': 'Git/Subversion', 'total': '90%'},
-        {'name': 'Google Analytics', 'total': '90%'},
-        {'name': 'Amazon Web Services', 'total': '85%'},
-        {'name': 'Photoshop', 'total': '90%'},
-        {'name': 'Illustrator', 'total': '50%'},
-        {'name': 'Wordpress', 'total': '85%'},
-        {'name': 'cross-browser/legacy testing', 'total': '98%'}
+        {
+            name: 'PHP',
+            total: '95%'
+        },
+        {
+            name: 'Ruby/Ruby on Rails',
+            total: '60%'
+        },
+        {
+            name: 'HTML5/CSS',
+            total: '99%'
+        },
+        {
+            name: 'JavaScript',
+            total: '80%'
+        },
+        {
+            name: 'Linux/Apache',
+            total: '70%'
+        },
+        {
+            name: 'MySQL',
+            total: '85%'
+        },
+        {
+            name: 'Git/Subversion',
+            total: '90%'
+        },
+        {
+            name: 'Google Analytics',
+            total: '90%'
+        },
+        {
+            name: 'Amazon Web Services',
+            total: '85%'
+        },
+        {
+            name: 'Photoshop',
+            total: '90%'
+        },
+        {
+            name: 'Illustrator',
+            total: '50%'
+        },
+        {
+            name: 'Wordpress',
+            total: '85%'
+        },
+        {
+            name: 'cross-browser/legacy testing',
+            total: '98%'
+        }
     ],
     clients: [
         {
             name: 'BBC',
             logo: 'bbc'
+        },
+        {
+            name: 'South West Trains',
+            logo: 'south-west-trains'
         },
         {
             name: 'Prezzo',
