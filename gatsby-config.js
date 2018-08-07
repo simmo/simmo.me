46b51522244d4313aabcc618eaed576f18ee8624
Mike Simmonds <simmo@users.noreply.github.com>
2018-08-07 11:54:07 +0100

    feature/2018 refresh (#1)
    * Removing old site and adding foundation for v3
    
    * Moving styles
    
    * Ignoring public folder
    
    * Removing github plugin
    
    * Updating packages
    
    * Removing unused packages, renaming js to jsx and fixing styles import
    
    * Adding favicons and GTM
    
    * Adding lang attribute
    
    * License update

---
+++ gatsby-config.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+
+module.exports = {
+  siteMetadata: {
+    title: 'Mike Simmonds - Lead Front-end developer',
+    siteUrl: 'https://simmo.me',
+    description: 'Lead front-end developer, living in Sussex and working in London, UK.',
+    keywords: [
+      'javascript',
+      'react',
+      'redux',
+      'mike',
+      'simmonds',
+      'london',
+      'html',
+      'css',
+      'simmo',
+      'developer',
+    ],
+    social: {
+      linkedin: 'http://lnkd.in/2T4KbK',
+      github: 'http://github.com/simmo',
+      stackoverflow: 'http://stackoverflow.com/users/547345/mike',
+      instagram: 'http://instagram.com/mikesimmonds',
+      twitter: 'http://twitter.com/mikesimmonds',
+    },
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: process.env.GOOGLE_TAG_MANAGER_ID,
+      },
+    },
+  ],
+}
+
+// {
+//   resolve: 'gatsby-source-github',
+//   options: {
+//     headers: {
+//       Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+//     },
+//     queries: [
+//       `{
+//           viewer {
+//             repositories(first: 30) {
+//               pageInfo {
+//                 hasNextPage
+//                 endCursor
+//               }
+//               edges {
+//                 node {
+//                   id
+//                   name
+//                   projectsUrl
+//                   description
+//                 }
+//               }
+//             }
+//           }
+//       }`,
+//     ],
+//   },
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Home from '../components/Home'
+import '../styles/index.module.css'
+
+export default () => (
+  <Layout>
+    <Home />
+  </Layout>
+)
