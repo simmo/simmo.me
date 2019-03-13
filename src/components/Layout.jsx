a20040e55bd8b5a092b05d2dd6b104579bd08c69
Mike Simmonds <mike@simmo.me>
2019-03-13 14:21:35 +0000

    Using props instead of components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -13,23 +13,30 @@ export default function Layout({ children }) {
 
   return (
     <>
-      <Helmet titleTemplate={'%s - {meta.title}'} defaultTitle={title}>
-        <html lang="en-GB" />
-        <meta name="author" content="Mike Simmonds" />
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords.join()} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="apple-mobile-web-app-title" content="simmo.me" />
-        <meta name="application-name" content="simmo.me" />
-        <meta
-          name="google-site-verification"
-          content="GbkERB2aLUOjt7IgBkrHpE61G7awq9zVYAu7ODXAfiA"
-        />
-        <link rel="apple-touch-icon" href={favIconApple} />
-        <link rel="icon" href={favIcon} />
-        <link rel="mask-icon" href={favIconMask} color="#111111" />
-        <link rel="preconnect" href="https://use.typekit.net" />
-      </Helmet>
+      <Helmet
+        {...{
+          defaultTitle: title,
+          htmlAttributes: { lang: 'en-GB' },
+          meta: [
+            { name: 'author', content: 'Mike Simmonds' },
+            { name: 'description', content: description },
+            { name: 'keywords', content: keywords.join() },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { vname: 'apple-mobile-web-app-title', content: 'simmo.me' },
+            { name: 'application-name', content: 'simmo.me' },
+            {
+              name: 'google-site-verification',
+              content: 'GbkERB2aLUOjt7IgBkrHpE61G7awq9zVYAu7ODXAfiA',
+            },
+          ],
+          link: [
+            { rel: 'apple-touch-icon', href: favIconApple },
+            { rel: 'icon', href: favIcon },
+            { rel: 'mask-icon', href: favIconMask, color: '#111111' },
+            { rel: 'preconnect', href: 'https://use.typekit.net' },
+          ],
+        }}
+      />
       <main className={styles.grid}>{children}</main>
     </>
   )
